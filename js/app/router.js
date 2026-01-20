888ff74f2b52c7c71730713dc2ded529311745ac
траур <vovchik200679@gmail.com>
2026-01-20 15:41:06 +0200

    Add files via upload

--- js/app/router.js
+++ js/app/router.js
@@ -1,28 +1,34 @@
-import { login } from './pages/login.js';
-import { campaigns } from './pages/campaigns.js';
-import { campaign } from './pages/campaign.js';
-import { users } from './pages/users.js';
-import { user } from './pages/user.js';
-import { ads } from './pages/ads.js';
-import { statistics } from './pages/statistics.js';
-import { payments } from './pages/payments.js';
-import { sites } from './pages/sites.js';
+// Вместо импортов - используем глобальные переменные
 
-export const router = VueRouter.createRouter({
+// Если компоненты будут определены в app.js
+const routes = [
+  { path: '/', name: 'Sign in', component: null }, // будет заменено
+  { path: '/campaigns', name: 'Campaigns', component: null },
+  { path: '/campaign/:id', name: 'Campaign', component: null },
+  { path: '/users', name: 'Users', component: null },
+  { path: '/user/:id', name: 'User', component: null },
+  { path: '/ads', name: 'Ads', component: null },
+  { path: '/statistics', name: 'Statistics', component: null },
+  { path: '/payments', name: 'Payments', component: null },
+  { path: '/sites', name: 'Sites', component: null }
+];
+
+// Создаем роутер и делаем его глобальным
+window.router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
-  routes: [
-    { path: '/', name: 'Sign in', component: login },
-
-    { path: '/campaigns', name: 'Campaigns', component: campaigns },
-    { path: '/campaign/:id', name: 'Campaign', component: campaign },
-
-    { path: '/users', name: 'Users', component: users },
-    { path: '/user/:id', name: 'User', component: user },
-
-    { path: '/ads', name: 'Ads', component: ads },
-
-    { path: '/statistics', name: 'Statistics', component: statistics },
-    { path: '/payments', name: 'Payments', component: payments },
-    { path: '/sites', name: 'Sites', component: sites }
-  ]
+  routes
 });
+
+// Функция для обновления компонентов (будет вызвана из app.js)
+window.setRouteComponents = function(components) {
+  window.router.removeRoute('/');
+  window.router.addRoute({ path: '/', name: 'Sign in', component: components.login });
+  window.router.addRoute({ path: '/campaigns', name: 'Campaigns', component: components.campaigns });
+  window.router.addRoute({ path: '/campaign/:id', name: 'Campaign', component: components.campaign });
+  window.router.addRoute({ path: '/users', name: 'Users', component: components.users });
+  window.router.addRoute({ path: '/user/:id', name: 'User', component: components.user });
+  window.router.addRoute({ path: '/ads', name: 'Ads', component: components.ads });
+  window.router.addRoute({ path: '/statistics', name: 'Statistics', component: components.statistics });
+  window.router.addRoute({ path: '/payments', name: 'Payments', component: components.payments });
+  window.router.addRoute({ path: '/sites', name: 'Sites', component: components.sites });
+};
