906df8638092788312edcd29a3c13374e8d07b77
траур <vovchik200679@gmail.com>
2026-01-26 16:02:13 +0200

    Add files via upload

--- js/app/widgets/header.js
+++ js/app/widgets/header.js
@@ -1,22 +1,73 @@
 export const header = {
-  data: function () {
-    return {
-      user: {},
-      parent: "",
-      active: 0,
-      menu: 0
-    };
-  },
-  watch: {
-  },
-  mounted() {
-    this.parent = this.$parent.$parent;
-  },
-  methods: {
-  },
-  template: `
-    <header class="header">
-      <msg ref="msg"/>
-    </header>
-  `
+    data: function () {
+        return {
+            user: {},
+            parent: "",
+            active: 0,
+            menu: 0
+        }
+    },
+    watch: {
+    },
+    mounted() {
+        let current = this.$parent;
+        while (current && !current.user) {
+            current = current.$parent;
+        }
+        this.parent = current;
+    },
+    methods: {
+        toggleActive() {
+            if (this.active == 1) {
+                this.active = 0;
+            } else {
+                this.active = 1;
+            }
+        }
+    },
+    template: `
+        <header class="header">
+            <div class="wrapper">
+                <div class="flex">
+                    <div class="w20 logo">
+                        <img src="./images/favicon.ico" />
+                    </div>
+                    <div class="w70">
+                        <div class="menu-user-container">
+                            <!-- User-circle теперь здесь, перед меню -->
+                            <div id="user-top" v-if="parent && parent.user && parent.user.user">
+                                <div id="user-circle" @click="toggleActive()">{{parent.user.user[0]}}</div>
+                                <div id="user-info" :class="{active:active==1}">
+                                    <a href="#" @click.prevent="parent.logout();"><i class="fas fa-sign-out-alt"></i> {{parent.user.user}} Log out</a>
+                                </div>
+                                <i @click="toggleActive()" class="fas fa-caret-down"></i>
+                            </div>
+                            
+                            <!-- Меню теперь справа от user-circle -->
+                            <div id="menu">
+                                <i class="fas fa-bars" @click="menu=1"></i>
+                                <ul :class="{active:menu==1}" v-if="parent && parent.user && parent.user.type && parent.user.type=='admin'">
+                                    <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
+                                    <li><router-link :class="{'router-link-active':$route.path.search('user')==1}" to="/users"><i class="fas fa-users"></i> Users</router-link></li>
+                                    <li><router-link :class="{'router-link-active':$route.path.search('campaign')==1}" to="/campaigns"><i class="fas fa-bullhorn"></i> Campaigns</router-link></li>
+                                </ul>
+
+                                <ul :class="{active:menu==1}" v-if="parent && parent.user && parent.user.type && parent.user.type!='admin'">
+                                    <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
+                                    <li><router-link to="/statistics"><i class="fas fa-chart-area"></i> Statistics</router-link></li>
+                                    <li><router-link to="/ads"><i class="fas fa-image"></i> Ads</router-link></li>
+                                    <li><router-link to="/sites"><i class="fab fa-chrome"></i> Sites</router-link></li>
+                                    <li><router-link to="/payments"><i class="fas fa-credit-card"></i> Payments</router-link></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w10 al">
+                        <!-- Это место теперь пустое или для других элементов -->
+                    </div>
+                </div>
+            </div>
+            <msg ref="msg"/>
+        </header>
+    `
 };
--- js/app/widgets/toogle.js
+++ js/app/widgets/toogle.js
@@ -1,9 +1,33 @@
-export const toogle = {
-  template: `
-    <label class="toggle">
-      <input type="checkbox" :checked="value" @change="$emit('update:value', $event.target.checked)">
-      <span class="toggle-slider"></span>
-    </label>
-  `,
-  props: ['value']
+export var toogle = {
+    data: function() {
+        return {
+            checked: false
+        }
+    },
+    watch: {
+        modelValue: function(newVal) {
+            // Преобразуем строку в булево значение
+            this.checked = newVal === "1" || newVal === true || newVal === "true";
+        }
+    },
+    mounted() {
+        // Инициализируем начальное состояние
+        this.checked = this.modelValue === "1" || this.modelValue === true || this.modelValue === "true";
+    },
+    methods: {
+        change() {
+            // Отправляем "1" для true, "0" для false
+            const value = this.checked ? "1" : "0";
+            this.$emit('update:modelValue', value);
+        }
+    },
+    props: {
+        modelValue: [String, Boolean] // Принимаем и строку и булево
+    },
+    template: `
+        <label class="switch">
+            <input type="checkbox" v-model="checked" @change="change()">
+            <span class="slider round"></span>
+        </label>
+    `
 };
